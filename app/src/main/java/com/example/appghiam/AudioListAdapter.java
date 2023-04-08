e87c2d2619f0169ac8ee1b8930124e7e76c41925
KingIT123 <kingitthe@gmail.com>
2023-04-08 21:18:10 +0700

    add onItemListClick and AudioViewHolder in AudioListAdapter

--- app/src/main/java/com/example/appghiam/AudioListAdapter.java
+++ app/src/main/java/com/example/appghiam/AudioListAdapter.java
@@ -10,21 +10,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.appghiam.AudioListFragment;
+
 import java.io.File;
 
 public class AudioListAdapter extends RecyclerView.Adapter<AudioListAdapter.AudioViewHolder> {
-    ///////////////////////////////////////////////////trường commit
     private File[] allFiles;
-
-    //private TimeAgo timeAgo;
+    private TimeAgo timeAgo;
     private onItemListClick onItemListClick;
 
-    public AudioListAdapter(File[] allFiles, onItemListClick onItemListClick) {
+    public AudioListAdapter(File[] allFiles, AudioListFragment onItemListClick) {
         this.allFiles = allFiles;
         this.onItemListClick = onItemListClick;
     }
 
-    ///////////////////////////////////////////////////HIẾU commit
     @NonNull
     @Override
     public AudioViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -41,14 +40,45 @@ public class AudioListAdapter extends RecyclerView.Adapter<AudioListAdapter.Audi
 
     @Override
     public int getItemCount() {
-
         return allFiles.length;
+    }
+
+    public interface onItemListClick {
+        void onClickListener(File file, int position);
+
+        void onDeleteClickListener(int position);
+
     }
 
     public class AudioViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private ImageView list_image;
         private TextView list_title;
         private TextView list_date;
-        ///////////////////////////////////////////////////SƠN commit
+        private ImageButton delete;
 
+        private ImageButton edit_name_file;
+
+        @Override
+        public void onClick(View view) {
+
+        }
+
+        public AudioViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+
+
+            delete = itemView.findViewById(R.id.delete_btn);
+            delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.SCREEN_STATE_ON)
+                        onItemListClick.onDeleteClickListener(position);
+                }
+            });
+        }
+
+
+    }
 }
